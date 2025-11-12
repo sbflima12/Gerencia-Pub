45c7a04656e46d6ed390ca577f71164d9a855cbb
Arthur Luiz Lopes Araujo <arthur.luizlopesaraujo@gmail.com>
2025-11-11 23:10:22 -0300

    feat(utils): adiciona o menu

---
+++ src/utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include "pedido.h"
+#include "estoque.h"
+
+typedef struct {
+    char nome[50];
+    int quantidadeTotal;
+    float valorTotal;
+} ResumoProduto;
+
+FILE* abrirArquivoClientes(int modo);
+
+void menuInicial();
+void sair();
+void cadastrarCliente();
+void consultarCliente();
+void lerDadosRelatorio(Pedido **pedidos, size_t *numPedidos, Produto **itens, int **idsItens, size_t *numItens, int filtrar, const char *dataFiltro);
+void gerarArquivoRelatorio(const char *dataFiltro, int filtrar, ResumoProduto *resumo, size_t numResumo, float totalVendas, size_t numPedidos);
+void gerarRelatorio();
+
+float calcularValorGasto(const char* buscaCpf);
+
+#endif
