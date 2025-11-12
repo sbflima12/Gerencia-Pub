ab80cf73dcee87ef9b9e22f100b193e489f74b05
Arthur Luiz Lopes Araujo <arthur.luizlopesaraujo@gmail.com>
2025-11-11 22:54:40 -0300

    .src/main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h" 
+
+
+int main()
+{
+    system("chcp 65001 > NUL"); //para permitir acentuação e caracteres especiais
+    menuInicial();
+
+    return 0;
+
+}
